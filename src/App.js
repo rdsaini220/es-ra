e79562bd47f054cfa92b7289e96ba06275f27121
radhetailwebs <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>
2022-09-18 00:17:25 +0530

    data fetch show table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import './App.css'
 
 function App() {
+  let [data, setData] = useState([]);
+
+  const getMeta = async () => {
+    var metaData = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo`).then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `This is an HTTP error: The status is ${response.status}`
+        );
+      }
+      return response.json()
+    })
+    setData(metaData)
+  }
+  useEffect(() => {
+    if (data.length === 0) {
+      getMeta()
+    }
+  }, [data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+          {
+            data.length !== 0 ?
+              <>                
+                  {
+                    Object.keys(data).map((item, ind) => {
+                      return (<>
+                        {
+                          ind === 0 ? 
+                            <div>
+                              {
+                                Object.keys(data[item]).map((i) => {
+                                  return (<h3>{i} :- {data[item][i]}</h3>)
+                                })
+                              }
+                            </div>                           
+                          : <table>                         
+                              {
+                                Object.keys(data[item]).map((i, index) => {            
+                                  return (<>
+                                    {  
+                                      index === 0 ? 
+                                      <thead>
+                                          <tr>
+                                            <th>DateTime</th>
+                                            {
+                                              Object.keys(data[item][i]).map((row) => {
+                                                return (<th>{row.split(" ")[1]}</th>)
+                                              })
+                                            }
+                                          </tr>
+                                      </thead> : '' 
+                                    }
+                                    <tbody>
+                                      <tr>
+                                        <td>{i}</td>
+                                        {
+                                          Object.keys(data[item][i]).map((row) => {
+                                            return (<td>{data[item][i][row]}</td>)
+                                          })
+                                        }
+                                      </tr>
+                                    </tbody> 
+                                  </>
+                                  )
+                                })
+                              } 
+                          </table>
+                        }
+                      </>)
+                    })
+                  }
+              </>
+              : <div>Loading</div>
+          }
+    </section>
   );
 }
 
